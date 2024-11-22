7209e7ba183bec4fa8c842c3d26c033d8515bda6
sirius-unity-2021 <sirius.unity.2021@mail.ru>
2024-11-22 05:05:52 +0300

    refactoring after check

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,13 +7,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.services.CarService;
+import web.services.CarServiceImpl;
 
 import java.util.List;
 
 @Controller
 public class CarController {
+    private final CarService carService;
+
     @Autowired
-    private CarService carService;
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
